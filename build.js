5c431cfc8e8d824d323ba5a804d2e83336c14b51
Rio6 <rio6.liu@gmail.com>
2020-06-18 21:17:10 -0700

    Nested directory support and WIP navigation menu

--- build.js
+++ build.js
@@ -9,23 +9,11 @@ require('showdown-highlightjs-extension');
 const READ_DIR = './blog';
 const WRITE_DIR = './dist';
 const TEMPLATE = './template.html';
+const BLOG_ROOT = process.env.BLOG_ROOT || '/';
 
 const langName = {
     en: "English",
     'zh-TW': "繁體中文"
-};
-
-let makeHTML = (template, title, content, langs, lang) => {
-    let document = new JSDOM(template).window.document;
-    document.getElementById('title').innerHTML = title;
-    document.getElementById('content').innerHTML = content;
-    document.getElementById('lang-select').innerHTML =
-        langs.map(l => l && `
-            <option value="${l}" ${l === lang ? 'selected disabled' : ''}>
-                ${langName[l] || l}
-            </option>
-        ` || '').reduce((a,c) => a+c, '');
-    return document.documentElement.outerHTML;
 };
 
 if(!fs.existsSync(WRITE_DIR)) {
@@ -44,78 +32,120 @@ let template = fs.readFileSync(TEMPLATE, 'utf-8');
 let tempStat = fs.statSync(TEMPLATE);
 let progStat = fs.statSync(process.argv[1]);
 
-let posts = [];
-let changed = false;
+let files = glob.sync(path.join(READ_DIR, '**/*.md'));
+let pageNavs = {}, pages = [];
 
 // Blog posts
-let files = glob.sync(path.join(READ_DIR, '*.md'));
 for(let file of files) {
 
     let data = fs.readFileSync(file, 'utf-8');
-    let name = path.basename(file).replace(/\.md$/, '');
-    let target = path.join(WRITE_DIR, name + '.html');
+    let rel = path.relative(READ_DIR, file);
+    let target = rel.replace(/\.md$/, '.html');
+    let navs = rel.split('/');
+
+    let page = pageNavs;
+    for(let nav of navs) {
+        if(!page[nav]) {
+            page[nav] = {};
+        }
+        page = page[nav];
+    }
+    pages.push(page);
 
     let content = converter.makeHtml(data)
 
     let postDom = new JSDOM(content).window.document;
     let title = postDom.getElementById('title')?.innerHTML;
     let date = postDom.getElementById('date')?.innerHTML;
-    let image = postDom.getElementsByTagName('img')[0]?.src;
 
     let getLang = file => file.match(/(\.([^.]+))*\.md$/)[2];
     let lang = getLang(file);
     let langs = glob.sync(file.replace(/\.[^.]+\.md$/, '.*.md')).map(getLang);
 
-    posts.push({
-        name: name,
-        title: title,
-        image: image,
-        date: date,
-        lang: lang
-    });
-
-    // Only update file when the last modify time is older
-    let fstat = fs.existsSync(file) && fs.statSync(file) || {};
-    let tstat = fs.existsSync(target) && fs.statSync(target) || {};
-    if(Math.max(fstat.mtime, tempStat.mtime, progStat.mtime) < tstat.mtime) {
-        console.log("skip", file);
-        continue;
-    }
-    changed = true;
-
-    // write file
-    fs.writeFile(target, makeHTML(template, title, content, langs, lang), err => {
-        if(err) throw(err);
-        console.log(file, "->", target);
-    });
+    page.title = title || path.basename(file).replace(/(\..*){0,1}\.md/, '');
+    page.date = date;
+    page.lang = lang;
+    page.langs = langs;
+    page.source = file;
+    page.target = target;
+    page.content = content;
 }
 
-// Main page
-if(changed) {
-    let langs = posts.map(p => p.lang);
-    langs = langs.filter((l,i) => langs.indexOf(l) === i);
+// Navigation list
+let makeNavs = (nav, selfTarget, lang) => {
+    let isEnd = nav => !Object.values(nav).every(v => typeof(v) === 'object');
 
-    for(let lang of langs) {
-        if(!lang) continue;
-
-        let content = "";
-
-        for(let post of posts.filter(p => !p.lang || p.lang === lang).reverse()) { // File with bigger filename comes first
-            content += `
-                <div class="post">
-                    <a href="${post.name + '.html'}">
-                        <h1>${post.title}</h1>
-                        <h2>${post.date}</h2>
-                        ${post.image && `<img src="${post.image}" />` || ''}
-                    </a>
-                </div>
-            `;
+    if(isEnd(nav)) {
+        // Page-nav
+        if(nav.target === selfTarget) {
+            return `<span class="nav-name" >${nav.title}</span>`;
+        } else {
+            return `<a class="nav-page" href="/${nav.target}">${nav.title}</a>`;
         }
 
-        let target = path.join(WRITE_DIR, `index.${lang}.html`);
-        fs.writeFile(target, makeHTML(template, "Rio's Blog", content, langs, lang), err => {
-            if(err) throw(err);
-            console.log("index ->", target);
-        });
+    } else {
+
+        // Directory-nav
+        let html = '<ul>';
+        for(let [name, child] of Object.entries(nav)) {
+            if(lang && child.lang && child.lang !== lang) continue;
+            html += `
+                <li>
+                    ${!isEnd(child) ? `<span class="nav-name">${name}</span>` : ''}
+                    ${makeNavs(child, selfTarget, lang)}
+                </li>
+            `;
+        }
+        html += '</ul>';
+
+        return html;
     }
-} else console.log("skip index");
+}
+
+let makeHTML = page => {
+    let document = new JSDOM(template).window.document;
+
+    // Contents
+    document.getElementById('title').innerHTML = page.title;
+    document.getElementById('nav').innerHTML = makeNavs(pageNavs, page.target, page.lang);
+    document.getElementById('content').innerHTML = page.content;
+    document.getElementById('lang-select').innerHTML =
+        page.langs.map(l => l && `
+            <option value="${l}" ${l === page.lang ? 'selected disabled' : ''}>
+                ${langName[l] || l}
+            </option>
+        ` || '').reduce((a,c) => a+c, '');
+
+    // Change root of absolute paths
+    document.querySelectorAll("*[href^='/']").forEach(e => {
+        e.href = BLOG_ROOT + e.href;
+    });
+    document.querySelectorAll("*[src^='/']").forEach(e => {
+        e.src = BLOG_ROOT + e.src;
+    });
+
+    return document.documentElement.outerHTML;
+};
+
+for(let page of pages) {
+    let target = path.join(WRITE_DIR, page.target);
+
+    if(process.argv[2] !== 'rebuild') {
+        // Only update file when the last modify time is older
+        let fstat = fs.existsSync(page.source) && fs.statSync(page.source) || {};
+        let tstat = fs.existsSync(target) && fs.statSync(target) || {};
+        if(Math.max(fstat.mtime, tempStat.mtime, progStat.mtime) < tstat.mtime) {
+            console.log("skip writing", page.source);
+            continue;
+        }
+    }
+
+    // write file
+    fs.mkdir(path.dirname(target), {recursive: true}, err => {
+        if(err) throw(err);
+        fs.writeFile(target, makeHTML(page), err => {
+            if(err) throw(err);
+            console.log(page.source, "->", target);
+        });
+    });
+}
